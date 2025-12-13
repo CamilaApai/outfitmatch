95b4efc62098c18daf8941c85c8ffc2c9353bb79
Camila Apai <camilaapai@hotmail.com>
2025-12-13 18:03:38 -0300

    Social Media Img and Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
+import { ClosetComponent } from './pages/closet/closet.component';
+import { BuildOutfitComponent } from './pages/outfit-builder/outfit-builder.component';
+import { AttributionComponent } from './pages/attribution/attribution.component';
 
-
-import { HomepageComponent } from './homepage/homepage.component';
 
 export const routes: Routes = [
-  { path: '', component: HomepageComponent },        // Homepage component as the default route
-  { path: 'homepage', component: HomepageComponent } // Homepage component route
+  { path: '', component: HomepageComponent },       
+  { path: 'closet', component: ClosetComponent },
+  { path: 'build-outfit', component: BuildOutfitComponent },
+  { path: 'attribution', component: AttributionComponent }
 ];
-
-

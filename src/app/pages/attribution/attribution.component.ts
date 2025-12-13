7c2c2aacf0e476e49a12043628937f3d40fd0be2
Camila Apai <camilaapai@hotmail.com>
2025-12-13 17:51:56 -0300

    Attribution Complete

---
+++ src/app/pages/attribution/attribution.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-attribution',
+  imports: [],
+  templateUrl: './attribution.component.html',
+  styleUrl: './attribution.component.css'
+})
+export class AttributionComponent {
+
+}

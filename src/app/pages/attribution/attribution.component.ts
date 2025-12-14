731beb8044f393a231ce868720e793ae542b770a
Camila Apai <camilaapai@hotmail.com>
2025-12-14 01:06:19 -0300

    Attribution Component Complete

--- src/app/pages/attribution/attribution.component.ts
+++ src/app/pages/attribution/attribution.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-attribution',
   imports: [],
-  templateUrl: './attribution.component.html',
-  styleUrl: './attribution.component.css'
+  templateUrl: './attribution.component.html'
 })
 export class AttributionComponent {
 
